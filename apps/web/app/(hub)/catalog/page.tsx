5d87e31a444102041434788942132752c4240430
acyberschool <167988870+acyberschool@users.noreply.github.com>
2026-08-27 13:56:13 +0300

    Add public course catalogue route

---
+++ apps/web/app/(hub)/catalog/page.tsx
@@ -0,0 +1,7 @@
+import StorefrontCatalog from '@components/Acyberschool/StorefrontCatalog'
+
+export const dynamic = 'force-dynamic'
+
+export default function CatalogPage() {
+  return <StorefrontCatalog />
+}
