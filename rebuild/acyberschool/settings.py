2cc3ee13f4230fc8ce98e630b3e730614bb2a228
acyberschool <167988870+acyberschool@users.noreply.github.com>
2026-09-02 11:52:30 +0300

    Add role aware navigation context

--- rebuild/acyberschool/settings.py
+++ rebuild/acyberschool/settings.py
@@ -36,6 +36,7 @@ TEMPLATES = [{
         "django.template.context_processors.request",
         "django.contrib.auth.context_processors.auth",
         "django.contrib.messages.context_processors.messages",
+        "lms.context_processors.navigation",
     ]},
 }]
 WSGI_APPLICATION = "acyberschool.wsgi.application"
