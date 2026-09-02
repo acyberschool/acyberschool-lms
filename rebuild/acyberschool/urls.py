85ccd6d748fdf22d9aebb5649b3baf37c1d12a66
acyberschool <167988870+acyberschool@users.noreply.github.com>
2026-09-02 11:35:31 +0300

    Route invitations through secure redemption flow

--- rebuild/acyberschool/urls.py
+++ rebuild/acyberschool/urls.py
@@ -3,13 +3,13 @@ from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include, path
-from lms import views
+from lms import invite_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
-    path("join/<str:code>/", views.join_invitation, name="join_invitation"),
+    path("join/<str:code>/", invite_views.join_invitation, name="join_invitation"),
     path("", include("lms.urls")),
 ]
 
